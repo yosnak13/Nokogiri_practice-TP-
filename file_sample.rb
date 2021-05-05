a6ecb1293982ad5500d18bc30d385fa8071cb732
yosnak13 <yosnak13@gmail.com>
2021-05-05 16:33:42 +0900

    file_sampleを実行してhello.txtを生成

--- file_sample.rb
+++ file_sample.rb
@@ -1,3 +1,8 @@
-file = File.open('scrape.rb', 'r')
-puts file.read
+#(path, mode): pathにはファイルのパスを文字列で、
+# modeにはモードを文字列で指定。rは読み読み込みモードで開き、wは書き込みモードで開く
+# file = File.open('scrape.rb', 'r')
+# puts file.read
+# file.close
+file = File.open('hello.txt', 'w')
+file.write('Hello World.')
 file.close
