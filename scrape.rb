2486e61c972c1a4b53348751e0fa8211084cb55c
yosnak13 <yosnak13@gmail.com>
2021-05-05 16:39:05 +0900

    ファイル保存メソッドを記述。htmlファイル作成・保存に成功

--- scrape.rb
+++ scrape.rb
@@ -3,5 +3,9 @@ require 'net/http'
 # 対象サイトのURLを指定してHTMLを取得し標準出力に表示します
 url = 'https://masayuki14.github.io/pit-news/'
 uri = URI(url)
+
 html = Net::HTTP.get(uri)
-puts html
+
+file = File.open('pitnews.html', 'w')
+file.write(html)
+file.close
