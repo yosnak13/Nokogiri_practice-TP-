4b15c77f3538cf982b0675df23e5846619f07b4c
yosnak13 <yosnak13@gmail.com>
2021-05-05 16:57:23 +0900

    ファイルへの保存処理をメソッド化

--- scrape.rb
+++ scrape.rb
@@ -4,8 +4,18 @@ def get_from(url)
   Net::HTTP.get(URI(url))
 end
 
-html = get_from('https://masayuki14.github.io/pit-news/')
+def write_file(path, text)
+  file = File.open('path', 'w')
+  file.write(text)
+  file.close
+end
 
-file = File.open('pitnews.html', 'w')
-file.write(html)
-file.close
+html = get_from('https://masayuki14.github.io/pit-news/')
+# url = 'https://masayuki14.github.io/pit-news/'
+# uri = URI(url)
+# html = Net::HTTP.get(uri)
+
+write_file('pitnews.html', html)
+# file = File.open('pitnews.html', 'w')
+# file.write(html)
+# file.close
