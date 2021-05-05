3629e498a6ebc3d3d20c61e58ad4df3840076ea2
yosnak13 <yosnak13@gmail.com>
2021-05-05 17:05:38 +0900

    wirte_fileをさらにリファクタリング

--- scrape.rb
+++ scrape.rb
@@ -5,17 +5,18 @@ def get_from(url)
 end
 
 def write_file(path, text)
-  file = File.open('path', 'w')
-  file.write(text)
-  file.close
+  # file = File.open('path', 'w')
+  # file.write(text)
+  # file.close
+  File.open('path', 'w') { |file| file.write(text) }
 end
 
-html = get_from('https://masayuki14.github.io/pit-news/')
+# html = get_from('https://masayuki14.github.io/pit-news/')
 # url = 'https://masayuki14.github.io/pit-news/'
 # uri = URI(url)
 # html = Net::HTTP.get(uri)
 
-write_file('pitnews.html', html)
+write_file('pitnews.html', get_from('https://masayuki14.github.io/pit-news/'))
 # file = File.open('pitnews.html', 'w')
 # file.write(html)
 # file.close
