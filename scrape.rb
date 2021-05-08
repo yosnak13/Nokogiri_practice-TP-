1291a51c3ba3e84dc523f12da25186aa28724c91
yosnak13 <yosnak13@gmail.com>
2021-05-08 21:49:25 +0900

    ニュースを取得

--- scrape.rb
+++ scrape.rb
@@ -31,4 +31,19 @@ doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
 # sectionタグの要素を取得
 section = doc.xpath('/html/body/main/section[2]').first
 # カテゴリー名の取得
-category = section.xpath('./h6'),first.text
+# category = section.xpath('./h6').first.text
+
+contents = {category: nil, news: []}
+contents[:category] = section.xpath('./h6').first.text
+section.xpath('./div/div').each do |node|
+  # ニュースタイトル取得
+  title = node.xpath('./p/strong/a').first.text
+  # ニュースURL取得
+  url = node.xpath('./p/strong/a').first['href']
+
+  # 取得したデータを保存するhashオブジェクトをcontensに代入
+  news = {title: title, url: url}
+  contents[:news] << news
+end
+
+pp contents
